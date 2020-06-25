1cadb3ffe8f3f066919132dcd0b014f79b053550
Nick Young <nick.young698@gmail.com>
2020-06-25 00:57:16 -0700

    add space bar to jump

---
+++ space_to_jump_2d/Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public bool grounded = true;
+    private Rigidbody2D rb2d;
+    public float jumpPower;
+    private BoxCollider2D boxCollider2D;
+    [SerializeField] private LayerMask platformsLayerMask;
+
+    void Start()
+    {
+        rb2d = rb2d = GetComponent<Rigidbody2D> ();
+        boxCollider2D = transform.GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded()) {
+            rb2d.velocity = Vector2.up * jumpPower;
+        }
+    }
+
+    private bool IsGrounded() {
+        RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, 0.1f, platformsLayerMask);
+        return raycastHit2D.collider != null;
+    }
+}
