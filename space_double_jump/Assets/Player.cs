6761fc92ea39924ca5c0eff303fc08b3f9c56478
Nick Young <nick.young698@gmail.com>
2020-06-28 02:12:47 -0700

    add double jump mechanic

---
+++ space_double_jump/Assets/Player.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public bool grounded = true;
+    private Rigidbody2D rb2d;
+    public float jumpPower;
+    private BoxCollider2D boxCollider2D;
+    [SerializeField] private LayerMask platformsLayerMask;
+
+    private int jumpCount;
+    public int jumpCountMax;
+
+    void Start()
+    {
+        rb2d = rb2d = GetComponent<Rigidbody2D> ();
+        boxCollider2D = transform.GetComponent<BoxCollider2D>();
+        jumpCountMax = 2;
+    }
+
+    void Update()
+    {
+        if (IsGrounded()) {
+            jumpCount = 1;
+        }
+
+        if(Input.GetKey(KeyCode.Space)) {
+            if (IsGrounded()) {
+                rb2d.velocity = Vector2.up * jumpPower;
+
+            } else {
+                if(Input.GetKeyDown(KeyCode.Space)) {
+                    if (jumpCount < jumpCountMax) {
+                        rb2d.velocity = Vector2.up * jumpPower;
+                        jumpCount++;
+                    }
+                }
+            }
+        }
+    }
+
+    private bool IsGrounded() {
+        RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, 0.1f, platformsLayerMask);
+        return raycastHit2D.collider != null;
+    }
+}
