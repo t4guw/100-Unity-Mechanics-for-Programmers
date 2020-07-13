500337c074a16f70d3d1e6a7bbc05c797933008c
Nick Young <nick.young698@gmail.com>
2020-07-13 00:58:23 -0700

    add smooth camera follow

---
+++ programs/smooth_camera_follow/Assets/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        //transform.LookAt(target);
+    }
+}
