c5e9d4eaa5dde0959571f6d2f768e8dd99f50ce1
Nick Young <nick.young698@gmail.com>
2020-07-16 08:34:20 -0700

    add raycast shooting 2d

---
+++ programs/raycast_shooting_2d/Assets/Shoot.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            StartCoroutine(Fire());
+        }
+    }
+
+    IEnumerator Fire()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.up, transform.up);
+
+        if (hitInfo)
+        {
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, hitInfo.point);
+        } else
+        {
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, transform.position + transform.up * 100);
+        }
+
+        lineRenderer.enabled = true;
+
+        yield return new WaitForSeconds(0.02f);
+
+        lineRenderer.enabled = false;
+    }
+}
