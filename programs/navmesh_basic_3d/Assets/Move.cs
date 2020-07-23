f659a1288198a3407fd3a831d2b258094e1bc921
Nick Young <nick.young698@gmail.com>
2020-07-23 03:48:28 -0700

    add navmesh 3d

---
+++ programs/navmesh_basic_3d/Assets/Move.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] Transform destination;
+
+    NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if(navMeshAgent == null)
+        {
+            Debug.LogError("nav mesh agent component not attached");
+        } else
+        {
+            SetDestination();
+        }
+    }
+
+    private void SetDestination()
+    {
+        if (destination != null)
+        {
+            Vector3 targetVector = destination.transform.position;
+            navMeshAgent.SetDestination(targetVector);
+        }
+    }
+}
