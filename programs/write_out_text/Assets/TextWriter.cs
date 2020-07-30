4aca6ed78dc56cc9867548d4031e7a7de4672bf9
Nick Young <nick.young698@gmail.com>
2020-07-29 18:10:58 -0700

    add text writing effect mechanic

---
+++ programs/write_out_text/Assets/TextWriter.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TextWriter : MonoBehaviour
+{
+    private Text uiText;
+    private string textToWrite;
+    private float timePerCharacter;
+    private float timer;
+    private int characterIndex;
+    private bool invisibleCharacters;
+
+    public void AddWriter(Text uiText, string textToWrite, float timePerCharacter, bool invisibleCharacters)
+    {
+        this.uiText = uiText;
+        this.textToWrite = textToWrite;
+        this.timePerCharacter = timePerCharacter;
+        this.invisibleCharacters = invisibleCharacters;
+        characterIndex = 0;
+    }
+
+    private void Update()
+    {
+        if (uiText != null)
+        {
+            timer -= Time.deltaTime;
+            while (timer <= 0f)
+            {
+                timer += timePerCharacter;
+                characterIndex++;
+                string text = textToWrite.Substring(0, characterIndex);
+
+                if (invisibleCharacters)
+                {
+                    text += "<color=#00000000>" + textToWrite.Substring(characterIndex) + "</color>";
+                }
+
+                uiText.text = text;
+
+                if (characterIndex >= textToWrite.Length)
+                {
+                    uiText = null;
+                    return;
+                }
+            }
+        }
+    }
+}
