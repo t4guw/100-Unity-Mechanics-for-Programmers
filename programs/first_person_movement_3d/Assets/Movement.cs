ea3495aa7558671901791019f07303ee5a2a231e
Nick Young <nick.young698@gmail.com>
2020-11-12 21:54:57 -0800

    upload and update reset scene webgl

---
+++ programs/first_person_movement_3d/Assets/Movement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 10f;
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("a"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+        transform.position = pos;
+    }
+}
