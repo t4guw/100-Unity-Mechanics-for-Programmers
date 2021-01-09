43200fb1ea37b4d87d17125572531add2d35d5ee
Jacob Pierce <JPierce2626@gmail.com>
2021-01-08 16:34:15 -0800

    Uploaded Quit Application on Key Press

---
+++ programs/quit_application_on_key_press/Assets/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown("q") || Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
