3abf2f110eb1fbf832f6efd13dd051b4f5870081
Jacob Pierce <JPierce2626@gmail.com>
2021-05-07 22:34:47 -0700

    Sleep to Skip Time & Map Waypoints

---
+++ programs/map_waypoints/Assets/WaypointScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class WaypointScript : MonoBehaviour
+{
+    public GameObject map;
+    public GameObject waypoint;
+    Vector3 waypointLoc;
+    bool MapActive;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            if (MapActive)
+            {
+                map.SetActive(false);
+                MapActive = false;
+            }
+            else
+            {
+                map.SetActive(true);
+                MapActive = true;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Delete))
+        {
+            waypoint.SetActive(false);
+        }
+
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 50f);
+        if (hit.collider != null)
+        {
+            if (Input.GetMouseButtonDown(0) && hit.collider.name == "Map")
+            {
+                waypointLoc = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                waypointLoc.z = 0f;
+                waypoint.transform.position = waypointLoc;
+                waypoint.SetActive(true);
+            }
+        }
+    }
+}
---
+++ programs/sleep_to_skip_time/Assets/SleepScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class SleepScript : MonoBehaviour
+{
+    public GameObject promptSleep;
+    public GameObject dropdowns;
+
+    bool nearBed;
+
+    void Start()
+    {
+        nearBed = false;
+    }
+
+    void Update()
+    {
+        if (nearBed && Input.GetKeyDown(KeyCode.E))
+        {
+            promptSleep.SetActive(false);
+            dropdowns.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Player")
+        {
+            promptSleep.SetActive(true);
+            nearBed = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.tag == "Player")
+        {
+            promptSleep.SetActive(false);
+            dropdowns.SetActive(false);
+            nearBed = false;
+        }
+    }
+}
